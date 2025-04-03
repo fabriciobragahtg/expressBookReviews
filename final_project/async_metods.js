f6341b3a3f6057cf43045449ec05eabbd01ba3c9
Fabricio Braga <fabribraga@gmail.com>
2025-04-03 18:00:33 +0100

    the base project, working as epxected

--- final_project/async_metods.js
+++ final_project/async_metods.js
@@ -1,24 +1,47 @@
 const getAllBooksCallback = (callback) => {
     setTimeout(() => {
-      const books = require('./router/booksdb.js');
-      callback(null, books);
+        const books = require('./router/booksdb.js');
+        callback(null, books);
     }, 1000); // simulate async operation
-  };
-  
+};
 
-  const getBookByISBNPromise = (isbn) => {
+
+const getBookByISBNPromise = (isbn) => {
     const books = require('./router/booksdb.js');
     return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const book = books[isbn];
+            if (book) resolve(book);
+            else reject("Book not found");
+        }, 1000);
+    });
+};
+
+
+const getBooksByAuthorAsync = async (author) => {
+    const books = require('./router/booksdb.js');
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const result = Object.values(books).filter(b =>
+                b.author.toLowerCase().includes(author.toLowerCase())
+            );
+            resolve(result);
+        }, 1000);
+    });
+};
+
+const getBooksByTitleAsync = async (title) => {
+    const books = require('./router/booksdb.js');
+  
+    return new Promise((resolve) => {
       setTimeout(() => {
-        const book = books[isbn];
-        if (book) resolve(book);
-        else reject("Book not found");
+        const result = Object.values(books).filter(b =>
+          b.title.toLowerCase().includes(title.toLowerCase())
+        );
+        resolve(result);
       }, 1000);
     });
   };
   
-  // Usage:
-  getBookByISBNPromise("2")
-    .then(book => console.log("Book by ISBN with Promise:\n", book))
-    .catch(err => console.error(err));
   
